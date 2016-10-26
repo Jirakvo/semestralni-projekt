e43754efefe125e7e17aedf87a5ba21a2a246482
jirakvo1 <jirakvo1@J09-17.uhk.cz>
2016-10-26 15:35:23 +0200

    Initial GUI commit

---
+++ src/cz/uhk/fim/pro2/gui/HomeScreen.java
@@ -0,0 +1,8 @@
+package cz.uhk.fim.pro2.gui;
+
+public class HomeScreen extends Screen {
+	
+	public HomeScreen(MainFrame mainFrame){
+		super(mainFrame);
+	}
+}
---
+++ src/cz/uhk/fim/pro2/gui/Screen.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.pro2.gui;
+
+import javax.swing.JPanel;
+
+public abstract class Screen extends JPanel {
+	
+	protected MainFrame mainFrame;
+	
+	public Screen(MainFrame mainFrame){
+		this.mainFrame = mainFrame;
+	}
+	
+}
