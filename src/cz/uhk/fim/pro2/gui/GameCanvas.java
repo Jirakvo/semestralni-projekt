927dc7f9f52e8756a82512e7c89cfe634d5ab9ae
jirakvo1 <jirakvo1@J09-18.uhk.cz>
2016-11-02 16:36:28 +0100

    Pridani elementu tube a hearts do gamescreen, vytvoreni GameCanvas pro vykreslovani

---
+++ src/cz/uhk/fim/pro2/gui/GameCanvas.java
@@ -0,0 +1,28 @@
+package cz.uhk.fim.pro2.gui;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import cz.uhk.fim.pro2.game.model.World;
+
+public class GameCanvas extends Canvas {
+
+	private World world;
+	
+	public GameCanvas(World world){
+		this.world = world;	
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		
+		
+		g.setColor(Color.RED);
+		g.fillRect((int) world.getBird().getPositionX() - 25, (int) world.getBird().getPositionY() - 25, 50, 50);
+		
+	}
+	
+	
+}
--- src/cz/uhk/fim/pro2/gui/GameScreen.java
+++ src/cz/uhk/fim/pro2/gui/GameScreen.java
@@ -7,6 +7,11 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 
+import cz.uhk.fim.pro2.game.model.Bird;
+import cz.uhk.fim.pro2.game.model.Heart;
+import cz.uhk.fim.pro2.game.model.Tube;
+import cz.uhk.fim.pro2.game.model.World;
+
 public class GameScreen extends Screen {
 	
 	public GameScreen(MainFrame mainFrame){
@@ -14,6 +19,7 @@ public class GameScreen extends Screen {
 		
 		JButton jButtonPause = new JButton("Pause");
 		JButton jButtonBack = new JButton("Back");
+		
 		
 		jButtonBack.addActionListener(new ActionListener() {
 			
@@ -28,10 +34,25 @@ public class GameScreen extends Screen {
 		jButtonPause.setBounds(400, 20, 60, 60);
 		
 		jButtonBack.setFont(new Font("Arial", Font.PLAIN, 10));
-		jButtonBack.setForeground(Color.CYAN);
+		jButtonBack.setForeground(Color.GREEN);
 		
 		
 		add(jButtonPause);
 		add(jButtonBack);
+		
+		//WORLD
+		Bird bird = new Bird("Ptak", 240, 400);
+		World world = new World(bird);
+		world.addTube(new Tube(400, 400, Color.red));
+		world.addTube(new Tube(600, 350, Color.red));
+		world.addTube(new Tube(800, 530, Color.red));
+		
+		world.addHeart(new Heart(500, 380));
+		world.addHeart(new Heart(700, 480));
+		
+		GameCanvas gameCanvas = new GameCanvas(world);
+		gameCanvas.setBounds(0, 0, MainFrame.WIDTH, MainFrame.HEIGHT);
+		add(gameCanvas);
+		
 	}
 }
